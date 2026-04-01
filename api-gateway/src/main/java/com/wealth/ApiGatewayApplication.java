effe36ea4d90de5c00f2472b19f05e169045a187
Vibhanshu Agarwal <vibhanshu.agarwal@gmail.com>
2026-04-02 00:48:27 +0530

    Refactor API Gateway: migrate route configuration to MVC-style routing and remove rate limiter setup

--- api-gateway/src/main/java/com/wealth/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/wealth/ApiGatewayApplication.java
@@ -2,9 +2,7 @@ package com.wealth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -12,9 +10,4 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
-    @Bean
-    public KeyResolver userKeyResolver() {
-        // Simple rate limiting by IP address for now
-        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
-    }
 }
