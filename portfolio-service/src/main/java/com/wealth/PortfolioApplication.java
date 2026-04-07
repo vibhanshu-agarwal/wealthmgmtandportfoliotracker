cf3d0f838178ea341259b3d5c04e59f5ea2dc505
Vibhanshu Agarwal <vibhanshu.agarwal@gmail.com>
2026-04-07 15:48:18 +0530

    feat: enable asynchronous processing in PortfolioApplication

--- portfolio-service/src/main/java/com/wealth/PortfolioApplication.java
+++ portfolio-service/src/main/java/com/wealth/PortfolioApplication.java
@@ -2,8 +2,10 @@ package com.wealth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PortfolioApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
